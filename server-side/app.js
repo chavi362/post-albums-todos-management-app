9b2ad2685cbed30333780d7b202fe8be0648155f
chavi362 <140444217+chavi362@users.noreply.github.com>
2024-05-01 20:07:29 +0300

    sfdh

--- server-side/app.js
+++ server-side/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use (express.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const port = 3000;
-const usersRouter=require("./routes/usersRouter")
-app.use("/users",usersRouter);
+const usersRouter = require("./routes/usersRouter"); // Assuming correct path
+
+
+app.use("/users", usersRouter);
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
+  console.log(`Example app listening on port ${port}`);
+});
+
+const router = express.Router();
+const { create, getByEmail, getById } = require('../controllers/usersController');
+
+router.route('/')
+  .post(create);
+
+module.exports = router;
--- server-side/routes/usersRouter.js
+++ server-side/routes/usersRouter.js
@@ -1,21 +1,21 @@
+
 const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/usersController')
-router.use (express.json());
-router.use(express.urlencoded({ extended: true }));
+const {create,getByEmail,getById} = require('../controllers/usersController')
+router.route('/')
+    .post(create);
+// router.get("/:id", async(req, res) => {
+//     const id = req.params.id;
+//     const toy = await controller.getById(id);
+//     res.send(toy)
+// });
 
-router.get("/:id", async(req, res) => {
-    const id = req.params.id;
-    const toy = await controller.getById(id);
-    res.send(toy)
-});
-
-router.post("/", async(req, res) => {
-    try{//name,userName,email,address,password,phone,company
-        const response=await controller.create(req.body.name,req.body.userName,req.body.email,req.body.address,req.body.password,req.body.phone,req.body.company)
-        res.send(await (response.insertId));
-    }catch(err){
-        res.sendFile(path.join(__dirname, '../public', 'error.html'));
-    }
+// router.post("/", async(req, res) => {
+//     try{//name,userName,email,address,password,phone,company
+//         const response=await controller.create(req.body.name,req.body.userName,req.body.email,req.body.address,req.body.password,req.body.phone,req.body.company)
+//         res.send(await (response.insertId));
+//     }catch(err){
+//         res.sendFile(path.join(__dirname, '../public', 'error.html'));
+//     }
    
-});
+// });
