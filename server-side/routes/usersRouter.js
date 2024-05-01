eb241b8601da2acf29be6aa4e441aa01d75629a6
chavi362 <140444217+chavi362@users.noreply.github.com>
2024-05-01 18:33:15 +0300

    adding userRoute

--- server-side/routes/usersRouter.js
+++ server-side/routes/usersRouter.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/toysController')
+const controller = require('../controllers/usersController')
 router.use (express.json());
 router.use(express.urlencoded({ extended: true }));
-const { } = require('../DB')
+
+router.get("/:id", async(req, res) => {
+    const id = req.params.id;
+    const toy = await controller.getById(id);
+    res.send(toy)
+});
+
+router.post("/", async(req, res) => {
+    try{//name,userName,email,address,password,phone,company
+        const response=await controller.create(req.body.name,req.body.userName,req.body.email,req.body.address,req.body.password,req.body.phone,req.body.company)
+        res.send(await (response.insertId));
+    }catch(err){
+        res.sendFile(path.join(__dirname, '../public', 'error.html'));
+    }
+   
+});
